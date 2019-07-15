083b49d889505b255e4d284a3f3e60948f990a5c
unknown <sammitra47@gmail.com>
2019-07-15 21:54:17 +0300

    minor changes

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {Query} from 'react-apollo';
+import {css} from '@emotion/core';
 import {SEARCH_QUERY} from '../Queries';
 import Preview from '../Preview/Preview';
 
 export const Search = ({param}) => {
     return (
-        <div>
-            <h2>results for {param}:</h2>
+        <div
+            css={css`
+                @media (min-width: 75em) {
+                    margin-left: 1.4em;
+                }
+            `}>
+            <h2
+                css={css`
+                    font-family: var(--id-font);
+                `}>
+                results for {param}:
+            </h2>
             <Query query={SEARCH_QUERY} variables={{param}} fetchPolicy="network-only">
                 {({data, loading}) => {
                     const {search} = data;
